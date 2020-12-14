ccfee30861ab5157cd7d54df0011575ee8059dea
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2020-12-14 20:16:04 +0300

    fix: take into account edge case i.e one of the digit is greater than 9

--- codility/valid_times.py
+++ codility/valid_times.py
@@ -1,20 +1,23 @@
-# Given 4 digits, find the number of 
-# possible time in a 24 hours format
+# Given 4 positive integers. Your task is to 
+# return the total number of VALID TIME that 
+# can be displayed on a digital clock (24 HOUR FORMAT)
+# Return 0 if no VALID TIME can be displayed
 
 import itertools
 
 def validate(seq):
-    # earliest ~ 00:00 | latest ~ 23:59
+    # NOTE: valid time is between 00:00 and 23:59
     return (seq[0] * 10 + seq[1]) < 24 and (seq[2] * 10 + seq[3]) < 60
 
-def f(*args): # -> f(A, B, C, D) ~ we need four values
+def f(*args): # ~> f(A, B, C, D)
+    # sanity check -- none of the digit should be greater than 9:
+    if [d for d in [*args] if d > 9]: return 0
     times = list(set(itertools.permutations([*args], 4))) # list of possible times
     return len([t for t in times if validate(t)])
-
 
 if __name__ == '__main__':
     # let's "test" our solution -- use py.test next time, please:-)
     print(f'{f(1, 2, 3, 4)}')
     print(f'{f(0, 0, 0, 0)}')
-    print(f'{f(12, 45, 0, 1)}')
+    print(f'{f(3, 8, 11, 1)}')
     print(f'{f(0, 5, 7, 2)}')
