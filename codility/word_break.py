cc16eb3c2f892ffe311a55c53aea1a485db56495
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2021-02-09 17:14:39 +0300

    update 'word_break' problem

--- codility/word_break.py
+++ codility/word_break.py
@@ -1,29 +1,33 @@
 """Given a non-empty string s and a dictionary word_dict 
 containing a list of non-empty words, determine if s can be segmented into 
 a space-separated sequence of one or more dictionary words.
+
 Note:
   - The same word in the dictionary may be reused multiple times in the segmentation. 
   - You may assume the dictionary does not contain duplicate words.
+
 Example 1:
     Input : s = 'leetcode', word_dict = ['leet', 'code']
     Output: True
+
 Example 2:
     Input : s = 'applepenapple', word_dict = ['apple', 'pen']
     Output: True
+
 Example 3
-    Input : s = 'catsanddog', word_dict = ['cats', 'dog', 'sand']
+    Input : s = 'catsanddog', word_dict = ['cats', 'dog', 'sand', 'cat']
     Output: False
 """
 
 def word_break(s, word_dict):
-    for word in word_dict:
-        if word not in s:
-            return False
-        s = s.replace(word, '')
-    # if s is empty then s can be segmented
-    return True if not s else False
+    tt = [True] + [False] * len(s) # truth-table
+    for index in range(1, len(tt)):
+        for word in word_dict:
+            if tt[index - len(word)] and s[:index].endswith(word):
+                tt[index] = True
+    return tt[-1]
 
 
 if __name__ == '__main__':
-    s, word_dict = 'applepenapple', ['apple', 'pen']
+    s, word_dict = 'cars', ['car', 'ca', 'rs']
     print(f'{s}, {word_dict}\nAnswer: {word_break(s, word_dict)}')
